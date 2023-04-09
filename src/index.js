0b71db95e816486c106ab351040e9a287c441963
louis-marceron <louis.marceron@protonmail.com>
2023-04-09 15:48:49 +0200

    refactor: require env variables to be set up
    In dev environment, an error is thrown if the .env file is missing.
    An error is thrown if the environment variable DATABASE_URL is missing.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
-// Empêche le programme de crasher en production, car il n'y pas dotenv
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
+    const result = require('dotenv').config();
+    if (result.error) {
+        throw new Error('Failed to load environment variables. You must create a .env file in the root directory of the project.');
+    }
 }
 
-const app = require('./app'); 
+if (!process.env.DATABASE_URL) {
+    throw new Error('Missing DATABASE_URL environment variable.');
+}
+
+const app = require('./app');
 const sequelize = require('./config/database');
 const PORT = process.env.PORT || 3000;
 
-// Charge les modelels de la base de données pour que Sequelize puisse les synchroniser
+// Load models
 require('./models/Book');
 
 (async () => {
-  try {
-      await sequelize.authenticate();
-      await sequelize.sync();
-      console.log('Database connection established and models synced.');
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        console.log('Database connection established and models synced.');
 
-      app.listen(PORT, () => {
-          console.log(`Server is running on port ${PORT}`);
-      });
-  } catch (error) {
-      console.error('Unable to connect to the database or sync models:', error);
-  }
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error('Unable to connect to the database or sync models:', error);
+    }
 })();
