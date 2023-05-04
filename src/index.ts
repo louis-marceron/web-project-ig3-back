74e44f247b9612e5406ffaca3f050c2e4e6ae8e4
louis-marceron <louis.marceron@protonmail.com>
2023-05-04 21:15:37 +0200

    fix: load devEnv and db before the server is listening

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import loadDevEnv from './config/loadDevEnv'
 import loadDatabase from './config/loadDatabase'
 import app from './app'
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Server is running: localhost.${PORT}`))
-
 loadDevEnv()
 loadDatabase()
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server is running: http://localhost:${PORT}`))
