252372c401cc9b897f2cb89a20bd2da963dfc84c
louis-marceron <louis.marceron@protonmail.com>
2023-05-28 11:53:25 +0200

    feat(user): allow the user to edit its own profil

--- src/middlewares/userIdMatchOrAdmin.middleware.ts
+++ src/middlewares/userIdMatchOrAdmin.middleware.ts
@@ -1,17 +1,19 @@
 import { Request, Response, NextFunction } from 'express'
 
-async function isAdmin(req: Request, res: Response, next: NextFunction) {
+async function userIdMatchUrlOrAdmin(req: Request, res: Response, next: NextFunction) {
     if (!req.user) {
         console.error('authenticateToken() middleware should be called before this middleware.')
         return res.status(500).json({ error: 'Internal server error.' })
     }
 
+    const idFromUrl = req.params.id
+    const idFromUser = req.user.id
     const isAdmin = req.user.is_admin
 
-    if (!isAdmin)
+    if (!isAdmin && idFromUser !== idFromUrl)
         return res.status(403).json({ error: 'You are not authorized to perform this action.' })
 
     next()
 }
 
-export default isAdmin
+export default userIdMatchUrlOrAdmin
--- src/routes/AppUser.route.ts
+++ src/routes/AppUser.route.ts
@@ -2,13 +2,14 @@ import { Router } from 'express'
 import * as Controller from '../controllers/AppUser.controller'
 import isAdmin from '../middlewares/isAdmin.middleware'
 import authenticateToken from '../middlewares/authenticateToken.middleware'
+import userIdMatchUrlOrAdmin from '../middlewares/userIdMatchOrAdmin.middleware'
 
 export default Router()
     .get('/', authenticateToken, isAdmin, Controller.getAllUsers)
-    .get('/:id', authenticateToken, isAdmin, Controller.getUserById)
+    .get('/:id', authenticateToken, userIdMatchUrlOrAdmin, Controller.getUserById)
     .post('/', authenticateToken, isAdmin, Controller.createUser)
-    .patch('/:id', authenticateToken, isAdmin, Controller.updateUser)
-    .delete('/:id', authenticateToken, isAdmin, Controller.deleteUser)
+    .patch('/:id', authenticateToken, userIdMatchUrlOrAdmin, Controller.updateUser)
+    .delete('/:id', authenticateToken, userIdMatchUrlOrAdmin, Controller.deleteUser)
     .post('/signup', Controller.signup)
     .post('/login', Controller.login)
     .post('/logout', authenticateToken, Controller.logout)
